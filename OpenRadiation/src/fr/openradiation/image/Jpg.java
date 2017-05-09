d0f16ebc127f0d2d21125bddc1d8c7698dcb9bc9
Pierre <Pierre@Pierre-PC>
2017-05-09 18:05:26 +0200

    add exceptions

--- OpenRadiation/src/fr/openradiation/image/Jpg.java
+++ OpenRadiation/src/fr/openradiation/image/Jpg.java
@@ -3,15 +3,18 @@ package fr.openradiation.image;
 import java.io.File;
 import java.io.IOException;
 
+import com.drew.imaging.ImageProcessingException;
+
 public class Jpg extends Img {
 	
 	/**
 	 * 
 	 * @param file
 	 * @throws IOException
+	 * @throws ImageProcessingException 
 	 */
 
-	public Jpg(File file) throws IOException {
+	public Jpg(File file) throws IOException, ImageProcessingException {
 		super(file);
 		// TODO Auto-generated constructor stub
 	}
--- OpenRadiation/src/fr/openradiation/image/Png.java
+++ OpenRadiation/src/fr/openradiation/image/Png.java
@@ -3,9 +3,11 @@ package fr.openradiation.image;
 import java.io.File;
 import java.io.IOException;
 
+import com.drew.imaging.ImageProcessingException;
+
 public class Png extends Img {
 
-	public Png(File file) throws IOException {
+	public Png(File file) throws IOException, ImageProcessingException {
 		super(file);
 		// TODO Auto-generated constructor stub
 	}
